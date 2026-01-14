11a8be42693525bd9714ef432907707afd4352ea
inhanbyeol94 <inhanbyeol94@gmail.com>
2026-01-14 13:00:14 +0900

    add support for writing SSM parameters to an environment file and optional environment variable loading

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,32 @@
 import { getInput, setFailed, setOutput, debug, exportVariable, setSecret } from '@actions/core';
 import { Service } from './service';
+import * as fs from 'node:fs';
 
 async function bootstrap() {
     try {
         const awsRegion = getInput('aws-region');
         const awsBasePath = getInput('aws-base-path');
-
+        const isLoadEnv = getInput('load-env') === 'true';
+        const envFileName = getInput('env-file-name');
         const service = new Service({ region: awsRegion });
 
         const res = await service.findAll(awsBasePath);
 
-        for (const parameter of res?.Parameters || []) {
-            if (!parameter.Name || !parameter.Value) return;
-            setSecret(parameter.Value);
-            exportVariable(service.transformKey(parameter.Name), parameter.Value);
+        if (isLoadEnv) {
+            for (const parameter of res?.Parameters || []) {
+                if (!parameter.Name || !parameter.Value) return;
+                setSecret(parameter.Value);
+                exportVariable(service.transformKey(parameter.Name), parameter.Value);
+            }
+        }
+
+        if (envFileName) {
+            const envFile = fs.createWriteStream(envFileName);
+            for (const parameter of res?.Parameters || []) {
+                if (!parameter.Name || !parameter.Value) return;
+                envFile.write(`${service.transformKey(parameter.Name)}="${parameter.Value}"\n`);
+            }
+            envFile.end();
         }
     } catch (error) {
         if (error instanceof Error) {
