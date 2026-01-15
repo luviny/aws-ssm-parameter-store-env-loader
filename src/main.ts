0bc89e2088e5b3afe12dedbf6e7aafe69d0618ee
inhanbyeol94 <inhanbyeol94@gmail.com>
2026-01-15 09:03:39 +0900

    set `gzip-env-value` as a secret to enhance security

--- src/main.ts
+++ src/main.ts
@@ -64,6 +64,7 @@ async function bootstrap() {
         const compressed = zlib.gzipSync(JSON.stringify(envObject));
         const base64 = compressed.toString('base64');
 
+        setSecret(base64);
         setOutput('gzip-env-value', base64);
     } catch (error) {
         if (error instanceof Error) {
